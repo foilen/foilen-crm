0d86d6bdd45a88171c84981bd4f026a01833592b
Simon Levesque <simon@foilen.com>
2019-12-28 21:25:07 -0500

    Add missing step instruction

--- test-crm-local.sh
+++ test-crm-local.sh
@@ -10,11 +10,13 @@ RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
 # Build
+echo '###[ Create build ]###'
 ./step-update-copyrights.sh
 ./step-compile-no-tests.sh
 ./step-create-docker-image.sh
 
 # Start
+echo '###[ Start UI ]###'
 USER_ID=$(id -u)
 docker run -ti \
   --rm \
