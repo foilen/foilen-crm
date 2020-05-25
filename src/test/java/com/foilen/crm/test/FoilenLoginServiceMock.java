4167a39d4c533e89ac3e9e4afde6f1a25dad48c0
Simon Levesque <simon@foilen.com>
2020-05-25 18:56:40 -0400

    Update copyrights

--- src/test/java/com/foilen/crm/test/FoilenLoginServiceMock.java
+++ src/test/java/com/foilen/crm/test/FoilenLoginServiceMock.java
@@ -1,7 +1,7 @@
 /*
     Foilen CRM
     https://github.com/foilen/foilen-crm
-    Copyright (c) 2015-2019 Foilen (http://foilen.com)
+    Copyright (c) 2015-2020 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
