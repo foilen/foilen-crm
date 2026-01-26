b78e2f344d73d4715ae7c4343f1105d90b2e067a
Simon Levesque <simon@foilen.com>
2026-01-26 13:14:01 -0500

    Upgrade libraries

--- src/main/java/com/foilen/crm/CrmUpgradesSpringConfig.java
+++ src/main/java/com/foilen/crm/CrmUpgradesSpringConfig.java
@@ -1,25 +1,22 @@
 package com.foilen.crm;
 
-import java.util.List;
-
+import com.foilen.smalltools.upgrader.UpgraderTools;
+import com.foilen.smalltools.upgrader.tasks.UpgradeTask;
+import com.foilen.smalltools.upgrader.trackers.DatabaseUpgraderTracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.foilen.smalltools.upgrader.UpgraderTools;
-import com.foilen.smalltools.upgrader.tasks.UpgradeTask;
-import com.foilen.smalltools.upgrader.trackers.DatabaseUpgraderTracker;
+import java.util.List;
 
 @Configuration
-@ComponentScan({ "com.foilen.crm.upgrades" })
-@EnableAutoConfiguration(exclude = { JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
-@PropertySource({ "classpath:/com/foilen/crm/application-common.properties", "classpath:/com/foilen/crm/application-${MODE}.properties" })
+@ComponentScan({"com.foilen.crm.upgrades"})
+@EnableAutoConfiguration
+@PropertySource({"classpath:/com/foilen/crm/application-common.properties", "classpath:/com/foilen/crm/application-${MODE}.properties"})
 public class CrmUpgradesSpringConfig {
 
     @Autowired
